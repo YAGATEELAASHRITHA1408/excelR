41a8de754e789914b6f8ab1783b1de8ac9afb6a9
YAGATEELAASHRITHA1408 <ashrithaashu0806@gmail.com>
2025-01-03 10:11:01 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,11 @@
+public class demo6 {
+    public static void main(String[] args) {
+        String username = "Ashritha001";
+        String password = "Ashritha_001";
+        while ((username == "Ashritha001") && (password == "Ashritha_001")) {
+            System.out.println("Hello Ashritha");
+            break;
+        }
+    }
+
+}
